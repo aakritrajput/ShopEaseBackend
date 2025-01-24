4e89766a89a00a63cb47e7c0d8f747f99be80db8
aakritrajput <aakritrajput87@gmail.com>
2025-01-24 17:12:35 +0530

    Add product routes for updating stock, deleting products, searching, and retrieving all products

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { addProduct } from "../controllers/product.controller.js";
+import { addProduct,  updateStock , deleteProduct, searchProduct, getAllProducts, getSellersAllProducts, getProduct, updateProductDetails } from "../controllers/product.controller.js";
 
 const router = Router();
 
-router.route("/addProduct").post(verifyJWT , upload.array('images', 7), addProduct)
+router.route("/addProduct").post(verifyJWT , upload.array('images', 7), addProduct);
+router.route("/updateStock/:productId").patch(verifyJWT, updateStock);
+router.route("/deleteProduct/:productId").delete(verifyJWT, deleteProduct);
+router.route("/searchProduct").get(verifyJWT, searchProduct);
+router.route("/allProducts").get(verifyJWT, getAllProducts);
+router.route("/sellersAllProducts/:sellerId").get(verifyJWT, getSellersAllProducts);
+router.route("/:productId").get(verifyJWT, getProduct);
+router.route("/updateProductDetails/:productId").patch(verifyJWT, updateProductDetails);
 
 export default router ;
